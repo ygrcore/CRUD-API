d092331d8224f9851078c5b34435eb1c68ff4406
Egor <itpd2010@yandex.ru>
2024-02-12 18:17:12 +0100

    feat: install and configure webpack

---
+++ webpack.config.ts
@@ -0,0 +1,23 @@
+const path = require('path');
+
+module.exports = {
+  target: 'node',
+  mode: 'development',
+  entry: './src/index.ts',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+};
